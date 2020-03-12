c4664a2f0ccecf384219650611ef92702502a0bc
muhannadjudah <muhannadjudah@gmail.com>
2020-03-12 11:09:50 +0100

    add xo files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Xo from "./components/XO"
 import './App.css';
 
 function App() {
+  const _ways = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+  const [ways, setWays] = useState(_ways)
+
+  const [player, setPlayer] = useState(0)
+
+  const myFunction = (index) => {
+    if (player === 0) {
+      ways[index - 1] = 1
+      document.getElementById(`div${index}`).innerHTML = "X";
+      setPlayer(1)
+    }
+    if (player === 1) {
+      ways[index - 1] = 0
+      document.getElementById(`div${index}`).innerHTML = "O";
+      setPlayer(0)
+    }
+    setWays(ways)
+
+    check()
+  }
+
+  const check = () => {
+    if (
+      (ways[0] === 1 && ways[1] === 1 && ways[2] === 1) ||
+      (ways[0] === 0 && ways[1] === 0 && ways[2] === 0)
+    ) {
+      if (player === 1) {
+        alert("the winner is O");
+      } else {
+        alert("the winner is X");
+      }
+    } else if (
+      (ways[3] === 1 && ways[4] === 1 && ways[5] === 1) ||
+      (ways[3] === 0 && ways[4] === 0 && ways[5] === 0)
+    ) {
+      if (player === 1) {
+        alert("the winner is O");
+      } else {
+        alert("the winner is X");
+      }
+    } else if (
+      (ways[6] === 1 && ways[7] === 1 && ways[8] === 1) ||
+      (ways[6] === 0 && ways[7] === 0 && ways[8] === 0)
+    ) {
+      if (player === 1) {
+        alert("the winner is O");
+      } else {
+        alert("the winner is X");
+      }
+    } else if (
+      (ways[0] === 1 && ways[3] === 1 && ways[6] === 1) ||
+      (ways[0] === 0 && ways[3] === 0 && ways[6] === 0)
+    ) {
+      if (player === 1) {
+        alert("the winner is O");
+      } else {
+        alert("the winner is X");
+      }
+    } else if (
+      (ways[1] === 1 && ways[4] === 1 && ways[7] === 1) ||
+      (ways[0] === 0 && ways[4] === 0 && ways[7] === 0)
+    ) {
+      if (player === 1) {
+        alert("the winner is O");
+      } else {
+        alert("the winner is X");
+      }
+    } else if (
+      (ways[2] === 1 && ways[5] === 1 && ways[8] === 1) ||
+      (ways[2] === 0 && ways[5] === 0 && ways[8] === 0)
+    ) {
+      if (player === 1) {
+        alert("the winner is O");
+      } else {
+        alert("the winner is X");
+      }
+    } else if (
+      (ways[0] === 1 && ways[4] === 1 && ways[8] === 1) ||
+      (ways[0] === 0 && ways[4] === 0 && ways[8] === 0)
+    ) {
+      if (player === 1) {
+        alert("the winner is O");
+      } else {
+        alert("the winner is X");
+      }
+    } else if (
+      (ways[2] === 1 && ways[4] === 1 && ways[6] === 1) ||
+      (ways[2] === 0 && ways[4] === 0 && ways[6] === 0)
+    ) {
+      if (player === 1) {
+        alert("the winner is O");
+      } else {
+        alert("the winner is X");
+      }
+    }
+  }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className="game">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSBZAieOkulioNBvVilYHTpY5wiLdDKDFeXHj8aKSJYgKOj9_mS" alt="lets-play"
+          />
+        </div>
+        <div className="container">
+          <div className="h1">
+            <h1>Tic Tac Toe</h1>
+          </div>
+          <div className="row border">
+            {ways.map((_, index) => <Xo key={index} index={index + 1} myId={"div" + (index + 1)} myFunction={myFunction} />)}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
